07d3fafb77da60ad0fa93aa33efda8e1df058d47
Benjamin Downs <benjamin.downs@gmail.com>
2016-02-26 22:10:19 +0100

    more ipaddress fixing

--- api/whoami.js
+++ api/whoami.js
@@ -3,7 +3,7 @@ module.exports = function(req, res) {
     var software = req.headers["user-agent"].match(/\((.*?)\)/)[1]
     var language = req.headers["accept-language"].split(',')[0]
     var headers = {
-        ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
         language: language, 
         software: software
     }
