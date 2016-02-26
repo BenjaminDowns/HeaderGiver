d21e538b9ffc13971c4be888175f5ca2b50045d2
Benjamin Downs <benjamin.downs@gmail.com>
2016-02-26 22:29:03 +0100

    fixed routing issues

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ var whoami = require('./api/whoami.js')
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', __dirname + 'public')
 
 app.get('/api/whoami', whoami)
 
+app.get('/whoami', function(req, res) {
+    res.redirect('api/whoami')
+})
+
 app.get('*', function(req, res) {
-    res.send('index')
+    res.redirect('/')
 })
 
 app.listen(port)
